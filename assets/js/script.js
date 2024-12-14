74eb33de9f5d479a02b80851495a058bb04dff83
AlejandroBattaQuin <156514282+AlejandroBattaQuin@users.noreply.github.com>
2024-12-14 23:38:49 +0100

    Optimizacion

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,7 @@
-/**
- * NAV
- */
-
-function submenu() {
-    var subnav = document.querySelector('.subnav-header');
-    if (subnav.style.display === 'none') {
-        subnav.style.display = 'flex';
-    } else {
-        subnav.style.display = 'none';
-    }
+function toggleMenu() {
+    const menu = document.querySelector('.header__menu');
+    menu.classList.toggle('header__menu--open');
 }
-
 
 /**
  * FOOTER
@@ -18,4 +9,4 @@ function submenu() {
 
 var copyright = new Date();
 var copy = copyright.getFullYear();
-document.querySelector('.copy').innerHTML = `&copy; ${copy} Colegio Santa María de Yermo`;
+document.querySelector('.footer__copy').innerHTML = `&copy; ${copy} Colegio Santa María de Yermo`;
